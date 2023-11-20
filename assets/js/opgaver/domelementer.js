127bf6d1c43719f936c4f5606c91828b9d273a5c
Speks2 <142408255+Speks2@users.noreply.github.com>
2023-11-20 10:35:47 +0100

    DOM-elementer
    addaddda

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -8,17 +8,36 @@ Du skal finde elementet med id="yellowFigure" og console logge det.
 */
 
 // din kode her
-
+let = document.getElementById('redFigure'); {
+    console.log('red figure');
+   
+}
+let = document.getElementById('yellowFigure'); {
+    console.log('yellow figure');
+   
+}
 
 /* opgave 1.2
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
 farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
 */
 
-
 // din kode her
-
-
+let = document.getElementById('redFigure'); {
+    console.log('red figure');
+    let elements = document.getElementsByClassName('myFigure'); 
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = 'blue'; 
+      }  
+}
+let = document.getElementById('yellowFigure'); {
+    console.log('yellow figure');
+    let elements = document.getElementsByClassName('myFigure'); 
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = 'blue'; 
+    } 
+   
+}
 
 /* opgave 2.1
 Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
@@ -28,6 +47,11 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 
 
 // din kode her
+let element = document.getElementById('domeelementer.html'); {
+    let elements = document.getElementsByClassName('h2'); 
+    let = document.getElementsByClassName('p'); 
+    console.log('Jeg har løst opgave 2.1?');
+}
 
 
 // opgave 3 liveHTML lists
@@ -36,7 +60,11 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
-
+let elements = document.getElementsByClassName('purpleFigures'); {
+    elementsArray.forEach.element('purpleFigures'); {
+         console.log(element);
+    } 
+}
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -45,7 +73,13 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 */
 
 // din kode her
-
+let = document.getElementsByClassName('purpleFigures'); {
+    let elementsArray = Array.from(elements);
+   
+    elementsArray.map(element => {
+  element.style.backgroundColor = 'red';
+});
+}
 
 /* opgave 3.3
 Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
@@ -54,6 +88,17 @@ myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 
 
 // din kode her
+let = document.getElementsByClassName('purpleFigures'); {
+    let elementsArray = Array.from(elements);
+    
+}
+
+elementsArray.forEach(element => {
+  let h3Element = element.children[1]; 
+  if (h3Element && h3Element.tagName === 'h3') { 
+    h3Element.innerHTML = 'RED'; 
+  }
+});
 
 
 
@@ -71,7 +116,25 @@ const myData = {
 
 
 // din kode her
+function createElementsFromData(myData) {
+    const container = document.getElementById('opgaveFour');
+    const article = document.createElement('article');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
 
+    h1.textContent = data.title;
+    p.textContent = data.description;
+
+    article.appendChild(h1);
+    article.appendChild(p);
+    container.appendChild(article);
+  }
+  createElementsFromData(myData);
+  
+
+    
+  
+  
 
 
 
